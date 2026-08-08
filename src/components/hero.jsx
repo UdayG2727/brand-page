ecdff75e49a91812969f20445051f09d45c15f9f
Uday Giri <udayg0848@gmail.com>
2026-08-08 16:50:41 +0530

    Restore firstweb

---
+++ src/components/hero.jsx
@@ -0,0 +1,28 @@
+const HeroSection=()=>{
+    return(<>
+    <main className="container">
+    <div className="hero">
+    <div className="hero-content">
+    <h1>YOUR FEET DESERVE THE BEST</h1>
+    <p>Experience the ultimate comfort and style with our premium footwear collection. Step into a world of unparalleled quality and fashion, where every pair is crafted to perfection. Your feet deserve nothing less than the best, and we're here to deliver it to you. </p>
+    <div className="hero-btn">
+    <button>Shop Now</button>
+    <button>Category</button>
+    </div>
+    <div>
+    <p>Also Available On</p>
+    <div className="brand-logos">
+    <img src="/images/brand1logo.png" alt="logo" height="50 px" width="50 px"/>
+    <img src="/images/brand2logo.png" alt="logo" height="50 px" width="50 px"/>
+    </div>
+    </div>
+    </div>
+    <div className="hero-image">
+    <img src="/images/samplepuma.png" alt="puma shoes" height="400px" width="500px"/>
+    </div>
+    </div>
+    </main>
+    </>
+    );
+}
+export default HeroSection;
