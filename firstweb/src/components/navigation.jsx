30c0f47ac4d11ccbe66f9f661219602c4ba76093
Uday Giri <udayg0848@gmail.com>
2026-08-08 16:35:35 +0530

    1st commit

---
+++ firstweb/src/components/navigation.jsx
@@ -0,0 +1,17 @@
+const Navigation=()=>{
+return(<>
+    <nav>
+        <div className="logo">
+        <img src="/images/brand_logo.png"alt="logo" height="50 px" width="50 px"/>
+        </div>
+        <ul>
+        <li href="#">Menu</li>
+        <li href="#">location</li>
+        <li href="#">Services</li>
+        <li href="#">Contact</li>
+        </ul>
+        <button>login</button>
+    </nav>
+    </>)
+}
+export default Navigation
